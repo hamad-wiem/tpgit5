6cf0c6afcb511efc8ca530fa66748863cb4af35a
hamad wiem <hamadwiem@gmail.com>
2025-10-17 16:58:52 +0200

    Ajout .gignore

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Print Hello, World! to the console !
         System.out.println("Hello, World!");
     }
 }
+ 
