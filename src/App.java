c82b6247d677ff11682721310241a445c420cc93
chaima askri <askrichayma7@gmail.com>
2025-10-17 17:15:53 +0200

    voisin chaima commit

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Print Hello, World! to the console !
         System.out.println("Hello, World!");
+        //it's chaima
     }
 }
  
